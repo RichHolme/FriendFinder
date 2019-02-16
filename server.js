1abcda448dac1071450f2021643a34e9918e66ab
Rich <rchrdholme@gmail.com>
2019-02-16 17:32:46 -0500

    favicon fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path =  require("path");
+var favicon = require('serve-favicon');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(favicon(path.join(__dirname,'app','public','spinach.png')));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
